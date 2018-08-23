739bfe64574df760969747ebff3817f1dfb98a2e
archibald1418 <35507021+archibald1418@users.noreply.github.com>
2018-08-23 17:42:20 +0300

    Update roman_numerals.py

--- roman_numerals.py
+++ roman_numerals.py
@@ -19,21 +19,21 @@ def razryady (n):
 
 def roman (digits):
 	'''Converts digits of a number starting from units'''
-	assoc = dict (zip ([0, 1, 5, 10, 50, 100, 500, 1000], ['', 'I','V','X','L','C','D','M'])) 
+	assoc = dict (zip ([0, 1, 5, 10, 50, 100, 500, 1000], # Basic terms mapping
+			   ['', 'I','V','X','L','C','D','M'])) 
 	output = ''
 	for i in range (len (digits)):# i denoting the current decimal place 
 		razryad = digits[i] 
-		scale = 10 ** i 						# Scaling 10 so it matches a basic term from the notation
+		scale = 10 ** i 			# Scaling 10 so it matches a given term from the notation
 		if razryad in (4, 5, 9):		# Special notation for 4 and 9. 5 needed to make distinction between 4 and 9.
 			output += assoc[scale * (razryad + int (10 % razryad != 0))] + 
 			(int ((razryad + 1) % 5 == 0)) * assoc[scale]
-																# Tedious juggling with numbers
-																# The booleans determine whether we should get 5 or 10 from the dict
+			# The boolean magic determines whether we should get 5 or 10 from the dict
 		else:
 			output += assoc[scale] * (razryad % 5) + assoc[scale * (razryad - (razryad % 5))]
 	return output[::-1]
 
-n = int(input("Введите число от 1 до 3999: "))
+n = int(input("Pick a number from 1 to 3999: "))
 
 digits = []
 
